9bb2761d0a81b0ad0a8b20c9fb385a02f02eb02d
Dhruv8703 <dhruvtiw@gmail.com>
2026-05-11 20:46:51 +0530

    initial commit

---
+++ game-backend-marketplace/src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private boolean enabled = false;
+
+    public Admin() {
+    }
+
+    public Admin(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.enabled = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
